e7a4e77064a21837c03deadc2bd01532e9091639
Ali <ali.nikfar2000@gmail.com>
2023-12-20 23:55:29 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "inbox" ,
+    durable:false , exclusive:false ,autoDelete: false , arguments:null);
+
+var message = " ";
+
+while (message != String.Empty)
+{
+    Console.Write("Please Enter Message Then Press Enter:");
+    message = Console.ReadLine();
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "", routingKey: "inbox", basicProperties: null, body);
+
+    Console.WriteLine("Message Sent to Client1");
+}
